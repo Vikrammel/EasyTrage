1b88653527b763a71686b11be0327c9439ece799
dunixify <vmelkote@ucsc.edu>
2018-02-04 22:39:10 -0800

    react redo working with npm start

---
+++ server.js
@@ -0,0 +1,46 @@
+//server.js
+'use strict'
+
+//first we import our dependencies…
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+//and create our instances
+var app = express();
+var router = express.Router();
+
+//set our port to either a predetermined port number if you have set 
+//it up, or 3001
+var port = process.env.API_PORT || 3001;
+
+//now we should configure the API to use bodyParser and look for 
+//JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+//now we can set the route path & initialize the API
+router.get('/', function(req, res) {
+ res.json({ message: 'API Initialized!'});
+});
+
+//Use our router configuration when we call /api
+app.use('/api', router);
+
+//starts the server and listens for requests
+app.listen(port, function() {
+ console.log(`api running on port ${port}`);
+});
