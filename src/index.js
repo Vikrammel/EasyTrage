603926976d1d1d34892c3fdd3084e3f543eab76a
dunixify <vmelkote@ucsc.edu>
2018-02-05 19:04:25 -0800

    fixed landing page load glitch and fixed react components file structure, will start to add in react router next

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Landing from './components/LandingComponent/Landing';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Landing />, document.getElementById('root'));
 registerServiceWorker();
