8f49f09b0f3eed441d9787a2505ffb8816e4bf5f
dunixify <vmelkote@ucsc.edu>
2018-02-07 00:01:16 -0800

    disabled input elements on form submit, working on email/pw validation

--- src/components/LandingComponent/GetStartedComponent/GetStarted.js
+++ src/components/LandingComponent/GetStartedComponent/GetStarted.js
@@ -5,11 +5,11 @@ import './GetStarted.css';
 class GetStarted extends Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    showLoginForm: false
-  };
-  this._onButtonClick = this._onButtonClick.bind(this);
+    super(props);
+    this.state = {
+      showLoginForm: false
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
   }
 
   _onButtonClick() {
--- src/components/LandingComponent/GetStartedComponent/Login.js
+++ src/components/LandingComponent/GetStartedComponent/Login.js
@@ -2,15 +2,54 @@ import React, {Component} from 'react';
 import './Login.css';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formDisabled: false
+    };
+    this._onSubmit = this._onSubmit.bind(this);
+  }
+  
+
+  _onSubmit = function(e){    
+    //prevent default onSubmit behavior (page reload/redirect)
+    e.preventDefault();
+    //disable text boxes
+    this.setState({formDisabled: true});
+    //validate email/pw
+    var email = this.refs["email"];
+    var password = this.refs["password"];
+    if(!email.includes('@')){
+      alert('invalid email');
+      this.setState({formDisabled: false});
+    }
+    else if (password.length < 6){
+      alert('password must be at least 6 characters');
+      this.setState({formDisabled: false});
+    }
+    else{
+      //send request for login/signup
+    }
+    
+
+    //redirect to dashboard if signin/up successful,
+    //else enable form fields again and display error
+  }
+
   render() {
     return (
         <div className='login'>
           <h3 id='loginHeader'>Account Details</h3>
-          <input name='username' placeholder='Username' type='text'/>
-          <input id='pw' name='password' placeholder='Password' type='password'/>
-          <input type='login' defaultValue='Sign in'/>
-          <hr />
-          <input type='signup' defaultValue='Sign up'/>
+          <form onSubmit={this._onSubmit}>
+            <input name='email' placeholder='email@domail.com' ref='email' 
+              type='text' disabled={this.state.formDisabled}/>
+            <input id='pw' name='password' placeholder='Password' ref='password' 
+              type='password' disabled={this.state.formDisabled}/>
+            <input id='login' type='submit' defaultValue='Sign in' ref='signin' />
+            <hr />
+            <input id='signup' type='submit' defaultValue='Sign up' ref='signup' />
+          </form>
         </div>
     );
   }
