c16b46e539d1d3d4f1258b641866eb5cc6835386
Abdul <aboodmm@gmail.com>
2018-02-08 20:23:01 -0800

    add login route and token generation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
 var User = require('./model/users');
 
@@ -25,9 +26,37 @@ router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!'});
 });
 
-router.get('/test', function(req, res) {
-  res.send("Testing my get");
+router.post('/login', function(req, res) {
+  const token = jwt.sign({ user: 'testuser' }, 'temp_pass');
+  res.json({
+    message: 'Authenticated! Use this token in the "authorization" header',
+    token: token
+  });
 });
+
+router.get('/protected', ensureToken, function(req, res) {
+  jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        description: 'Protected information. Congrats!'
+      });
+    }
+  });
+});
+
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 
 app.listen(port, function() {
