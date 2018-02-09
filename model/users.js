b3f1d5b404e3d3aac06f2453d58f782a26fa703d
vikrammel <vmelkote@ucsc.edu>
2018-02-08 20:21:12 -0800

    user model created for login/registration with getters and pw comparison function

--- model/users.js
+++ model/users.js
@@ -4,15 +4,46 @@
 
 //import dependency
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
 var Schema = mongoose.Schema;
 
 //create instance of mongoose.schema. Schema takes an object
 //that shows the shape/structure of db entries
 
-var UsersSchema = new Schema({
-    username: String,
-    email: String
-});
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {runSettersOnQuery: true} );
 
 //export module to use in server.js
-module.exports = mongoose.model('User', UsersSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserByEmail = function(email, callback){
+    const query = {email: email};
+    User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+module.exports.comparePassword = function(password, hash, callback){
+    bcrypt.compare(password, hash, (err, isMatch) => {
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
