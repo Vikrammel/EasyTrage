de48fdcc48c89fbe80d518552378d72007260660
James Kong <kong@usca028145m-2.local>
2018-02-25 18:45:34 -0800

    Updated

--- client/src/components/NavbarComponent/Navbar.js
+++ client/src/components/NavbarComponent/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+//import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar () {
     return (
-        <ul className='Navbar'>
-            <li>
-                <NavLink exact activeClassName='active' to="/">
-                    Home
-                </NavLink>
-                <NavLink activeClassName='active' to="/Dashboard">
-                    Dashboard
-                </NavLink>
-            </li>
+         <ul className='Navbar'>
+                <ul className="nav nav-pills">
+                    <li><a href="/">Home</a></li>
+                    <li className="active"><a href="/Dashboard">Dashboard</a></li>
+                    <li><a href="#">Trade History</a></li>
+                    <li><a href="#">Api Keys</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Account Settings</a></li>
+                </ul>
         </ul>
     )
 }
