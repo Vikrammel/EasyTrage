73ae774d71a98f9bef1b5e96baef38e7412e5d0a
dunixify <vmelkote@ucsc.edu>
2018-03-13 13:05:45 -0700

    added ensureToken and ensureUser function to backend auth, using it in /bot routes. Setting up front end to send a trade request on 'confirm' from modal

--- server/routes/bot.js
+++ server/routes/bot.js
@@ -3,20 +3,25 @@ const express = require('express');
 const router = express.Router();
 var axios = require('axios');
 var ccxt = require ('ccxt')
+const auth = require('./auth');
 
-//routes for /trade
-router.get('/trade/:exchange/:pair/:buysell/:price/:amount', function (req, res) {
-    res.json("trade requested on " + req.params.exchange + " to " + req.params.buysell
-        + " " + req.params.amount + " " + req.params.pair.slice(3) + " worth of " + 
-        req.params.pair.slice(0, 3) + " at " + req.params.price + " " + req.params.pair.slice(3) 
-        + "/" + req.params.pair.slice(0, 3));
-    // res.json(ccxt.exchanges);
+//routes for /bot/trade
+router.post('/trade', function (req, res) {
+    auth.ensureUser(req, (user)=> {
+        res.json(user.email + " requested to " + req.body.buysell
+            + " " + req.body.amount + " " + req.body.pair.slice(3) + " worth of " + 
+            req.body.pair.slice(0, 3) + " at " + req.body.price + " " + req.body.pair.slice(3) 
+            + "/" + req.body.pair.slice(0, 3) + " on " + req.body.exchange);
+        // res.json(ccxt.exchanges);
+    });
 });
 
-//routes for /trade/move
-router.get('/move/:ticker/:exchange1/:exchange2/:amount', function (req,res){
-    res.json("move " + req.params.amount + " " + req.params.ticker + " from " + req.params.exchange1 + 
-        " to " + req.params.exchange2 + " requested");
+//routes for /bot/move
+router.get('/move', function (req,res){
+    auth.ensureUser(req, (user)=> {
+        res.json(user.email + "requested to move " + req.body.amount + " " + req.body.ticker + 
+            " from " + req.body.exchange1 + " to " + req.body.exchange2 );
+    });
 });
 
 module.exports = router;
